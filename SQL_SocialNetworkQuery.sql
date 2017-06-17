31349685f8728ecd03fc259ed79a23c74d683509
cwang0129 <cwang0129@gmail.com>
2017-06-17 13:13:55 -0700

    adding SocialNetworkQuery

---
+++ SQL_SocialNetworkQuery.sql
@@ -0,0 +1,104 @@
+/* Delete the tables if they already exist */
+drop table if exists Highschooler;
+drop table if exists Friend;
+drop table if exists Likes;
+
+/* Create the schema for our tables */
+create table Highschooler(ID int, name text, grade int);
+create table Friend(ID1 int, ID2 int);
+create table Likes(ID1 int, ID2 int);
+
+/* Populate the tables with our data */
+insert into Highschooler values (1510, 'Jordan', 9);
+insert into Highschooler values (1689, 'Gabriel', 9);
+insert into Highschooler values (1381, 'Tiffany', 9);
+insert into Highschooler values (1709, 'Cassandra', 9);
+insert into Highschooler values (1101, 'Haley', 10);
+insert into Highschooler values (1782, 'Andrew', 10);
+insert into Highschooler values (1468, 'Kris', 10);
+insert into Highschooler values (1641, 'Brittany', 10);
+insert into Highschooler values (1247, 'Alexis', 11);
+insert into Highschooler values (1316, 'Austin', 11);
+insert into Highschooler values (1911, 'Gabriel', 11);
+insert into Highschooler values (1501, 'Jessica', 11);
+insert into Highschooler values (1304, 'Jordan', 12);
+insert into Highschooler values (1025, 'John', 12);
+insert into Highschooler values (1934, 'Kyle', 12);
+insert into Highschooler values (1661, 'Logan', 12);
+
+insert into Friend values (1510, 1381);
+insert into Friend values (1510, 1689);
+insert into Friend values (1689, 1709);
+insert into Friend values (1381, 1247);
+insert into Friend values (1709, 1247);
+insert into Friend values (1689, 1782);
+insert into Friend values (1782, 1468);
+insert into Friend values (1782, 1316);
+insert into Friend values (1782, 1304);
+insert into Friend values (1468, 1101);
+insert into Friend values (1468, 1641);
+insert into Friend values (1101, 1641);
+insert into Friend values (1247, 1911);
+insert into Friend values (1247, 1501);
+insert into Friend values (1911, 1501);
+insert into Friend values (1501, 1934);
+insert into Friend values (1316, 1934);
+insert into Friend values (1934, 1304);
+insert into Friend values (1304, 1661);
+insert into Friend values (1661, 1025);
+insert into Friend select ID2, ID1 from Friend;
+
+insert into Likes values(1689, 1709);
+insert into Likes values(1709, 1689);
+insert into Likes values(1782, 1709);
+insert into Likes values(1911, 1247);
+insert into Likes values(1247, 1468);
+insert into Likes values(1641, 1468);
+insert into Likes values(1316, 1304);
+insert into Likes values(1501, 1934);
+insert into Likes values(1934, 1501);
+insert into Likes values(1025, 1101);
+
+/*
+Students at your hometown high school have decided to organize their social network using databases. So far, they have collected information about sixteen students in four grades, 9-12. Here's the schema: 
+
+Highschooler ( ID, name, grade ) 
+English: There is a high school student with unique ID and a given first name in a certain grade. 
+
+Friend ( ID1, ID2 ) 
+English: The student with ID1 is friends with the student with ID2. Friendship is mutual, so if (123, 456) is in the Friend table, so is (456, 123). 
+
+Likes ( ID1, ID2 ) 
+English: The student with ID1 likes the student with ID2. Liking someone is not necessarily mutual, so if (123, 456) is in the Likes table, there is no guarantee that (456, 123) is also present. 
+
+*/
+
+
+#1 Find the names of all students who are friends with someone named Gabriel. 
+
+ select h.name from Highschooler h
+ where h.ID in
+ (
+ select f.ID1 as ID from Friend f where ID2 in (
+     select h.ID from Highschooler h 
+     where h.name ='Gabriel')
+  union 
+  select f.ID2 as ID from Friend f where ID1 in (
+     select h.ID from Highschooler h 
+     where h.name ='Gabriel')
+ )
+
+ #2 For every student who likes someone 2 or more grades younger than themselves, return that student's name and grade, and the name and grade of the student they like. 
+
+select h1.name, h1.grade, h2.name, h2.grade
+from Highschooler h1
+join Likes l on h1.ID = l.ID1
+join Highschooler h2 on h2.ID = l.ID2
+where h1.grade >= h2.grade + 2
+and h1.name <> h2.name;
+
+#3 For every pair of students who both like each other, return the name and grade of both students. Include each pair only once, with the two names in alphabetical order. 
+
+
+
+
